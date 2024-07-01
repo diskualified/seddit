9cf3ce1de98422f7c6e4e755978fa06431eb4527
David Qian <dqian@college.harvard.edu>
2024-07-01 10:56:03 -0400

    login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <p>Welcome to Seddit</p>
-      <button className="btn">Login</button>
+      <Link href="/login" className="btn">Login</Link>
     </main>
     
   );
