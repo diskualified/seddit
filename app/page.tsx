ddaca7943c490f1c99cf8fc844fc553b40f8879d
David Qian <dqian@college.harvard.edu>
2024-07-16 10:28:55 -0400

    add user email display - client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 // import { getServerSession } from "next-auth";
 // import { redirect } from "next/navigation";
 export default function Home() {
@@ -8,10 +8,13 @@ export default function Home() {
   // if (!session) {
   //   redirect("/login");
   // }
+  const { data: session } = useSession();
   return (
-    <div className="h-screen w-screen flex justify-center items-center m-0 p-0">
+    <div className="flex flex-col h-screen w-screen justify-center items-center m-0 p-0">
       <p>Welcome to Seddit</p>
-
+      <div>
+        <p>Email: {session?.user?.email}</p>
+      </div>
       <button className="btn" onClick={() => signOut()}>
         Logout
       </button>
